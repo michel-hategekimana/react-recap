046bdab4403a68b34eaa17da43978acf5abe3d36
chance-steven <michelhategekimana@127gmail.com>
2025-02-06 16:18:51 +0200

    set a rink to the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 const Button = () => {
   return (
     <div className="grid justify-center mt-3 mb-7">
-      <button className="bg-[#8247C9] px-5 py-2.5 rounded-3xl text-white text-sm hover:bg-[#F1C50C]">READ MORE</button>
+      <a href="https://www.zara.com/uk/en/search?v2=719022&searchTerm=floral%20print%20palazzo%20trousers&section=WOMAN">
+        <button className="bg-[#8247C9] px-5 py-2.5 rounded-3xl text-white text-sm hover:bg-[#F1C50C]">
+          READ MORE
+        </button>
+      </a>
     </div>
   );
 };
